10ce200dd9a30b114d1a1d7338b6bf5121b7a0e9
chinnari gara <garachinnari6@gmail.com>
2026-07-26 16:40:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
 print("APP STARTED")
 
 from flask import Flask, render_template, request
+from datetime import datetime
+import os
 
 app = Flask(__name__)
+
 
 @app.route("/", methods=["GET", "POST"])
 def home():
 
-    # Sensor values (demo input)
+    # Student count from website
     students_present = int(request.form.get("students", 30))
+
+    # Total classroom strength
+    total_students = 60
+
+    # Calculate absent students
+    students_absent = total_students - students_present
+
+    # Sensor value (demo)
     light_intensity = 40
 
     # Light automation logic
@@ -22,20 +33,23 @@ def home():
         suggestion = "Classroom empty. Switch OFF lights to save energy."
     else:
         suggestion = "Classroom active. Lights are controlled automatically."
-# Attendance log save
+
+    # Save attendance record
     with open("attendance.txt", "a") as file:
+        file.write(f"Time: {datetime.now()}\n")
         file.write(f"Students Present: {students_present}\n")
+        file.write(f"Students Absent: {students_absent}\n")
         file.write(f"Light Status: {light_status}\n")
         file.write("--------------------\n")
+
     return render_template(
         "index.html",
         light_status=light_status,
         students_present=students_present,
+        students_absent=students_absent,
         suggestion=suggestion
-    
-)
+    )
 
-import os
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))

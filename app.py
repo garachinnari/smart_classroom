25a9e0c82e18d16a2376dcf2169b67c4772b62de
chinnari gara <garachinnari6@gmail.com>
2026-07-26 16:31:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
 
     # Sensor values (demo input)
